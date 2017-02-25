a8b0a5971103cb90cef8d4f8e3ffce103b849d9f
Nimit Agg <nimitagg95@gmail.com>
2017-02-25 19:04:19 +0530

    feat: Dialog Set up

--- app/src/main/java/com/nimit/todo/MainActivity.java
+++ app/src/main/java/com/nimit/todo/MainActivity.java
@@ -1,13 +1,24 @@
 package com.nimit.todo;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    private FloatingActionButton add;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        add = (FloatingActionButton) findViewById(R.id.add);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/nimit/todo/database/DatabseColumns.java
@@ -0,0 +1,35 @@
+package com.nimit.todo.database;
+
+import net.simonvt.schematic.annotation.AutoIncrement;
+import net.simonvt.schematic.annotation.DataType;
+import net.simonvt.schematic.annotation.NotNull;
+import net.simonvt.schematic.annotation.PrimaryKey;
+
+public class DatabseColumns {
+    @DataType(DataType.Type.INTEGER)
+    @NotNull
+    @PrimaryKey
+    @AutoIncrement
+    public static final String ID = "_id";
+
+    @DataType(DataType.Type.TEXT)
+    @NotNull
+    public static final String ITEM = "item";
+
+    @DataType(DataType.Type.TEXT)
+    @NotNull
+    public static final String DESCRIPTION = "description";
+
+    @DataType(DataType.Type.TEXT)
+    @NotNull
+    public static final String URL = "url";
+
+    @DataType(DataType.Type.TEXT)
+    @NotNull
+    public static final String PRIORITY = "priority";
+
+    @DataType(DataType.Type.TEXT)
+    @NotNull
+    public static final String DUE = "due";
+
+}
